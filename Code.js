5be048530da48cdaabe735cb6c7034c2f36b87b3
William Sebastian <79944030+willcodes338@users.noreply.github.com>
2021-12-27 11:47:34 -0500

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "NATURE";
+const accessCode2 = "ANIMAL";
+const accessCode3 = "FOREST";
+
+function clues() {
+
+    fill("white");
+    textSize(15);
+    text("ETUNRA", displayWidth/3-100, displayHeight/2-125);
+
+    fill("black");
+    textSize(15);
+    text("HINT: NATURAL",displayWidth/3-100,displayHeight/2-105);
+
+    fill("white");
+    textSize(15);
+    text("NMILAA", displayWidth/2+340,displayHeight/2.2-40); 
+
+    fill("black");
+    textSize(15);
+    text("HINT: FOUND IN NATURE",displayWidth/2+340, displayHeight/2.2-20);
+
+    fill("white");
+    textSize(15);
+    text("FESTRO", displayWidth/2+40, displayHeight/2-70);
+
+    fill("black");
+    textSize(15);
+    text("HINT: TREES ARE FOUND HERE", displayWidth/2+40, displayHeight/2-50);
+}
